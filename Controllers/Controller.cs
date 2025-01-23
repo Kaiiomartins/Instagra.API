b21269ce84afbc9802e79d4f2b9e6739af9190d1
Kaio Martins <kaiomartins13@hotmail.com>
2025-01-23 19:02:46 -0300

    Initial commit

---
+++ Controllers/Controller.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Instagram.API.Data;
+using Instagram.API.Model;
+using System.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Identity.Client;
+
+namespace Instagram.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        private List<User> users = new List<User>();
+
+        [HttpGet]
+        public async Task<IActionResult> getUser(int id)
+        {
+
+            var user = users.FirstOrDefault(x => x.Id == id);
+
+            if (user == null) {
+                return BadRequest("Usuario não encontrado");
+            }
+            return Ok(user);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> createUser([FromBody] User user)  {
+
+            if (users.Any(u => u.Id == user.Id)) {
+                return BadRequest("Já existe esse funcionario");
+            }
+            users.Add(user);
+
+            return CreatedAtAction(nameof(getUser), new { id = user.Id }, user);
+}
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateUser([FromBody]User user) {
+
+            var usuario = users.FirstOrDefault(u => u.Id == user.Id);
+
+            if (usuario == null) {
+                return NotFound("Usuario não encontrado"); ;
+            }
+            usuario.UserName = user.UserName;
+            usuario.email = user.email;
+            usuario.DataNascimento = user.DataNascimento;
+            usuario.Passaword = user.Passaword;
+            
+            return Ok("Usuário atualizado com sucesso");
+
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete( int id) {
+
+
+            var usuario = users.FirstOrDefault(u => u.Id == id);
+
+            if (usuario == null) {
+                return NotFound("Usuario não encontrado");
+            }
+            users.Remove(usuario);
+
+            return Ok("Usuario deletado com sucesso");
+        }
+
+
+            
+        }
+
+    }
+
---
+++ Models/User.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Instagram.API.Model
+{
+    
+    [Table ("Users")]
+    public class User 
+    {
+        [Key]
+        public  int Id { get; set; }
+
+        [Required]
+        [StringLength(11)]
+        public string cpf { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string UserName { get; set; }
+
+
+        [Required]
+        [StringLength(255)]
+        public string Passaword { get; set; }
+
+        [StringLength(255)]
+        public string email { get; set;  }
+
+
+        public string DataNascimento { get; set; }
+
+        public DataSetDateTime CreatedAt { get; set; }
+
+        public   DateTime ? UpatetedAt { get; set; } = DateTime.Now;
+
+    }
+}
