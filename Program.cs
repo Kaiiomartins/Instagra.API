1641829bfe36bc98536f0e743da0c0368d9c4192
Kaio Martins <kaiomartins13@hotmail.com>
2025-05-27 18:43:53 -0300

    Alterada a versão dos pacotes, do .NET e ajustado o Swagger

--- Program.cs
+++ Program.cs
@@ -9,43 +9,37 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
-builder.Services.AddScoped<IUserRepository, UserRepository>();  
-builder.Services.AddScoped<IPostRepository, PostRepository>();  
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IPostRepository, PostRepository>();
 builder.Services.AddTransient<IUserService, UserService>();
 builder.Services.AddScoped<IPostService, PostsService>();
-
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowLocalhost",
-        builder => builder.WithOrigins("http://localhost:53773")
-                          .AllowAnyMethod()
-                          .AllowAnyHeader());
+        policy => policy.WithOrigins("http://localhost:53773")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
 });
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
-    {
-        Title = "Instagram API",
-        Version = "v1",
-        Description = "API Instagram"
-    });
-});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Instagram API v1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
-app.UseCors("AllowLocalhost"); 
+app.UseCors("AllowLocalhost");
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
